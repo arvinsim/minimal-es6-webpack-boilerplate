9e9c8e987e88f89ca78ba706b6d934b632b3cc28
Arthur Vincent Simon <asimon@innovuze.com>
2017-02-06 14:32:05 +0800

    Add packages and update webpack config
    - Add babel-core and babel-loader as
    devDependencies
    - Update webpack config to use babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,12 @@ module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
     }
-}
+};
